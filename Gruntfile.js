f31159d761181d6e95d7e5fac387b01ccaa8bc93
Zach Leatherman <zachleatherman@gmail.com>
2015-12-08 16:13:49 -0600

    Trying to get menu into npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,12 @@
 		// Project configuration.
 		grunt.initConfig({
 			// Metadata.
-			pkg: grunt.file.readJSON('menu.jquery.json'),
+			pkg: grunt.file.readJSON('package.json'),
 			banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
 				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 				'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
 				'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+				' Licensed <%= pkg.license %> */\n',
 			// Task configuration.
 			clean: {
 				files: ['dist']
@@ -23,24 +23,24 @@
 					stripBanners: true
 				},
 				distMenu: {
-					src: ['src/<%= pkg.name %>.js'],
-					dest: 'dist/<%= pkg.name %>.js'
+					src: ['src/<%= pkg.filename %>.js'],
+					dest: 'dist/<%= pkg.filename %>.js'
 				},
 				distMenuInit: {
-					src: ['src/<%= pkg.name %>-init.js'],
-					dest: 'dist/<%= pkg.name %>-init.js'
+					src: ['src/<%= pkg.filename %>-init.js'],
+					dest: 'dist/<%= pkg.filename %>-init.js'
 				},
 				distMenuTrigger: {
-					src: ['src/<%= pkg.name %>-trigger.js'],
-					dest: 'dist/<%= pkg.name %>-trigger.js'
+					src: ['src/<%= pkg.filename %>-trigger.js'],
+					dest: 'dist/<%= pkg.filename %>-trigger.js'
 				},
 				distMenuTriggerInit: {
-					src: ['src/<%= pkg.name %>-trigger-init.js'],
-					dest: 'dist/<%= pkg.name %>-trigger-init.js'
+					src: ['src/<%= pkg.filename %>-trigger-init.js'],
+					dest: 'dist/<%= pkg.filename %>-trigger-init.js'
 				},
 				distMenuAll: {
 					src: ['<%= concat.distMenu.dest %>', '<%= concat.distMenuInit.dest %>', '<%= concat.distMenuTrigger.dest %>','<%= concat.distMenuTriggerInit.dest %>' ],
-					dest: 'dist/<%= pkg.name %>-all.js'
+					dest: 'dist/<%= pkg.filename %>-all.js'
 				}
 			},
 			uglify: {
@@ -49,23 +49,23 @@
 				},
 				distMenu: {
 					src: ['<%= concat.distMenu.src %>'],
-					dest: 'dist/<%= pkg.name %>.min.js'
+					dest: 'dist/<%= pkg.filename %>.min.js'
 				},
 				distMenuInit: {
 					src: ['<%= concat.distMenuInit.src %>'],
-					dest: 'dist/<%= pkg.name %>-init.min.js'
+					dest: 'dist/<%= pkg.filename %>-init.min.js'
 				},
 				distMenuTrigger: {
 					src: ['<%= concat.distMenuTrigger.src %>'],
-					dest: 'dist/<%= pkg.name %>-trigger.min.js'
+					dest: 'dist/<%= pkg.filename %>-trigger.min.js'
 				},
 				distMenuTriggerInit: {
 					src: ['<%= concat.distMenuTriggerInit.src %>'],
-					dest: 'dist/<%= pkg.name %>-trigger-init.min.js'
+					dest: 'dist/<%= pkg.filename %>-trigger-init.min.js'
 				},
 				distMenuAll: {
 					src: ['<%= concat.distMenuAll.src %>'],
-					dest: 'dist/<%= pkg.name %>-all.min.js'
+					dest: 'dist/<%= pkg.filename %>-all.min.js'
 				}
 			},
 			qunit: {
